b0f287eb01750d6fe43148d53668ad096bbb6ddc
julien44830 <deniaudjulien0@gmail.com>
2024-05-27 00:15:42 +0200

    creation select/option & liste des stations *avant le fetch* stylisation scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import BackgroundWave from "./components/BackgroundWave";
+import MusicStyleSelect from "./components/MusicStytleSelect";
 import "./styles/global.css"
 
 function App() {
     return (
         <>
+            <MusicStyleSelect />
             <BackgroundWave />
         </>
     );
---
+++ src/components/MusicStytleSelect.jsx
@@ -0,0 +1,76 @@
+function MusicStyleSelect() {
+    return (
+        <section className="section-select-style-music">
+            <select
+                name=""
+                id=""
+                className="select-style-music"
+            >
+                <option value="">Pop</option>
+                <option value="">Rock</option>
+                <option value="">Hip-Hop/Rap</option>
+                <option value="">Electro/Dance</option>
+                <option value="">Jazz</option>
+                <option value="">Classique</option>
+                <option value="">Country</option>
+                <option value="">Reggae</option>
+                <option value="">Blues</option>
+                <option value="">Soual/Funk</option>
+                <option value="">Latine</option>
+                <option value="">Metal</option>
+            </select>
+
+            <div className="list-station">
+                <ul className="station-ul scroller">
+                    <a href="">
+                        <li className="station-li">station 1</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 2</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 3</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 4</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 5</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 6</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 7</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 8</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 9</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 10</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 11</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 12</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 13</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 14</li>
+                    </a>
+                    <a href="">
+                        <li className="station-li">station 15</li>
+                    </a>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default MusicStyleSelect;
