4c63497ff6bde869acdb0012b14c53c2748dc5df
julien44830 <deniaudjulien0@gmail.com>
2024-05-26 21:58:41 +0200

    background wave

---
+++ src/assets/components/TestBackground.jsx
@@ -0,0 +1,19 @@
+import '../styles/test-background.css'
+
+function TestBackground() {
+    return (
+        <video
+            autoPlay
+            muted
+            playsInline
+            loop
+        >
+            <source
+                src="https://res.cloudinary.com/shanomurphy/video/upload/v1520181785/water_obm8lb.webm"
+                type="video/webm"
+            />
+        </video>
+    );
+}
+
+export default TestBackground;
